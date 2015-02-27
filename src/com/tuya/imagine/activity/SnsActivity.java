d1aabc67fa1f8029db56f5bdaa735cd7fbdc8e36
panxiang <trop2008@hotmail.com>
2015-02-28 01:57:43 +0800

    Modify SNS ,remove QQ SINA...

--- src/com/tuya/imagine/activity/SnsActivity.java
+++ src/com/tuya/imagine/activity/SnsActivity.java
@@ -3,6 +3,7 @@
  * 创建人: d.cc
  * 创建时间: 2014-4-29
  */
+
 package com.tuya.imagine.activity;
 
 import java.io.File;
@@ -28,52 +29,47 @@ import com.tuya.imagine.app.BaseActivity;
  */
 public class SnsActivity extends BaseActivity implements OnClickListener {
 
-	private String mTempPath = "";
+    private String mTempPath = "";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_sns);
-		mTempPath = this.getIntent().getStringExtra(MyConstant.EXTRA_SAVE_PATH);
-		this.findViewById(R.id.btn_left).setOnClickListener(this);
-		this.findViewById(R.id.btn_right).setOnClickListener(this);
-		this.findViewById(R.id.btn_share2more).setOnClickListener(this);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sns);
+        mTempPath = this.getIntent().getStringExtra(MyConstant.EXTRA_SAVE_PATH);
+        this.findViewById(R.id.btn_left).setOnClickListener(this);
+        this.findViewById(R.id.btn_right).setOnClickListener(this);
+        this.findViewById(R.id.btn_share2more).setOnClickListener(this);
+    }
 
-	@Override
-	public void onClick(View v) {
+    @Override
+    public void onClick(View v) {
 
-		int id = v.getId();
-		if (id == R.id.btn_save) {
+        int id = v.getId();
+        if (id == R.id.btn_save) {
 
-		} else if (id == R.id.btn_left) {
-			SnsActivity.this.finish();
-		} else if (id == R.id.btn_right) {
-			Intent intent = new Intent();
-			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			intent.setClass(this, ImagineActivity.class);
-			this.startActivity(intent);
-		} else if (id == R.id.btn_share2more) {
-			if (!StringUtil.isEmptyString(mTempPath)) {
-				shareMore(new File(mTempPath));
-			}
-		}
-	}
+        } else if (id == R.id.btn_left) {
+            SnsActivity.this.finish();
+        } else if (id == R.id.btn_right) {
+            Intent intent = new Intent();
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.setClass(this, ImagineActivity.class);
+            this.startActivity(intent);
+        } else if (id == R.id.btn_share2more) {
+            if (!StringUtil.isEmptyString(mTempPath)) {
+                shareMore(new File(mTempPath));
+            }
+        }
+    }
 
-	public void shareMore(File newPath) {
-		AlertDialog.Builder alertDialog = new AlertDialog.Builder(
-				SnsActivity.this);
-		final ShareToAppListAdapter chooseAppAdapter = new ShareToAppListAdapter(
-				SnsActivity.this, "", Uri.fromFile(newPath));
-		alertDialog.setAdapter(chooseAppAdapter,
-				new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						SnsActivity.this
-								.startActivity((Intent) chooseAppAdapter
-										.getItem(which));
-					}
-				}).show();
-	}
+    public void shareMore(File newPath) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(SnsActivity.this);
+        final ShareToAppListAdapter chooseAppAdapter = new ShareToAppListAdapter(SnsActivity.this, "", Uri.fromFile(newPath));
+        alertDialog.setAdapter(chooseAppAdapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                SnsActivity.this.startActivity((Intent) chooseAppAdapter.getItem(which));
+            }
+        }).show();
+    }
 
 }
